f1b41bf2ab1a812c438bd609be5b22c74b18e91a
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-09 20:19:35 -0300

    atv4.java

---
+++ atv4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class atv4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Insira dois número:");
+        int num1 = s.nextInt();
+        int num2 = s.nextInt();
+
+        System.out.println(num1 + num2);
+
+        System.out.println();
+    }
+}
