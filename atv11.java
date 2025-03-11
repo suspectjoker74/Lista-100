0a908f2602eb5a257a01072b2e53f9543cf6d51f
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-11 17:48:55 -0300

    atv11.java

---
+++ atv11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class atv11 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Calculadora de bhaskara!\n Insira o valor de A, B e C. Respectivamente.");
+
+        double numA = s.nextFloat();
+        double numB = s.nextFloat();
+        double numC = s.nextFloat();
+
+        double delta = (numB*numB - 4*numA*numC);
+
+        double x1 = (-numB + Math.pow(delta, 0.5)) / (2 * numA);
+        double x2 = (-numB - Math.pow(delta, 0.5)) / (2 * numA);
+
+        System.out.println("Δ=" + delta);
+        System.out.println("x'=" + x1 + " " + "x''=" + x2);
+
+        s.close();
+    }
+
+}
