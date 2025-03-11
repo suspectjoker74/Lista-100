607fc15c840238b8d4a8a6301050b12790ac639c
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-11 17:49:48 -0300

    atv13.java

---
+++ atv13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class atv13 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Insira valor do salário:");
+        double salario = s.nextFloat();
+        
+        System.out.println("Insira valor da porcentagem aumento:");
+        double aumento = s.nextFloat();
+
+        aumento = Math.abs(((aumento-100)/100)+1);
+
+        System.out.println("Preco final: R$" + salario*(aumento+1) );
+
+        s.close();
+    }
+
+}
