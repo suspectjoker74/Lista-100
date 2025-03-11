cb4327d5d712ef3bb395a6e0d5026148a9e30636
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-11 19:51:47 -0300

    atv11.java

--- atv11.java
+++ atv11.java
@@ -12,12 +12,8 @@ public class atv11 {
 
         double delta = (numB*numB - 4*numA*numC);
 
-        double x1 = (-numB + Math.pow(delta, 0.5)) / (2 * numA);
-        double x2 = (-numB - Math.pow(delta, 0.5)) / (2 * numA);
-
         System.out.println("Δ=" + delta);
-        System.out.println("x'=" + x1 + " " + "x''=" + x2);
-
+        
         s.close();
     }
 
