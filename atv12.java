395a07c0a44574e4592e7985ec7d6b1b0a6854ac
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-11 19:49:01 -0300

    atv12.java

--- atv12.java
+++ atv12.java
@@ -7,12 +7,7 @@ public class atv12 {
         System.out.println("Insira valor do produto:");
         double preco = s.nextFloat();
         
-        System.out.println("Insira valor do desconto:");
-        double desconto = s.nextFloat();
-
-        desconto = Math.abs((desconto-100)/100);
-
-        System.out.println("Preco final: R$" + preco*desconto );
+        System.out.println("Preco final: R$" + (preco - (preco * 0.05)));
 
         s.close();
     }
