ee4de0120e6e7df97a8ffefab78023c07a6dcc2d
suspectjoker74 <104913183+suspectjoker74@users.noreply.github.com>
2025-03-11 17:51:57 -0300

    atv16.java

---
+++ atv16.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class atv16 {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        int mPC = 10; // minutes per cigarrete xD
+
+        System.out.println("Quantos cigarros voce fuma por dia?");
+        int cgrrDia = s.nextInt();
+
+        System.out.println("A quantos anos voce fuma?");
+        int anos = s.nextInt();
+
+        int diasFumados;
+        if (anos%4 == 0) diasFumados = (anos*365) + (anos/4); // considerando anos bissextos em média
+        else diasFumados = (anos*365);
+
+        int cigarrosFumados = cgrrDia*diasFumados;
+
+        System.out.println(diasFumados);
+        System.out.println(cigarrosFumados);
+        
+
+        s.close();
+    }
+
+}
